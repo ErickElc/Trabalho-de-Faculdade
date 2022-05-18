fcacdffa7d62eefe726d331b1b2280093ea5e414
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-05-18 19:00:28 -0300

    teste

--- scripts/add-produtos.js
+++ scripts/add-produtos.js
@@ -2,20 +2,18 @@
 const botao = document.getElementById("Incluir");
 const textarea = document.getElementById("quadro-branco");
 const TextPreco = document.getElementById("segundo-quadro");
+const opc1 = document.getElementById("op1");
 let select = document.getElementById("Produtos");
-
 let Salvar = [];
 let soma = 0;
 
 
 botao.addEventListener('click',()=>{
-    //TEXTO
-
         let opcaoTexto = select.options[select.selectedIndex].text;
         if(opcaoTexto == "---"){
             alert("Nenhum Produto selecionado!");
         }
-        // 
+        
         else{
             textarea.innerHTML+= (opcaoTexto + "\n");
             Salvar.push(parseInt(select.value));
@@ -24,11 +22,23 @@ botao.addEventListener('click',()=>{
             console.log(soma);
 
         };
-
-    ////SOMA
-
-
-
     })
-
+function Limpar(){
+    textarea.innerHTML="";
+    TextPreco.value="";
+    soma = 0;
     
+    alert("Limpando")
+
+}
+function Confimar(){
+    if(soma != 0){
+        textarea.innerHTML="";
+        TextPreco.value="";
+        alert("Obrigado por Comprar!!!")
+
+    }
+    else 
+        alert("Nenhum Produto selecionado")
+    soma = 0;
+}
